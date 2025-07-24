65c0194880dea42851bc2135f9583ff12d47ab97
Ckevyn Omar Ovalle Sequén <covalle@minfin.gob.gt>
2025-07-24 17:38:47 -0600

    (+)origns

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
-  // Otras configuraciones...
 
-  ...(process.env.NODE_ENV === "development" && {
-    allowedDevOrigins: [
-      "http://localhost:3000",
-      "http://167.88.46.48:3000",
-    ],
-  }),
-} as const;
+const nextConfig: NextConfig = {
+  /* config options here */
+  allowedDevOrigins: [
+    "http://167.88.46.48:3000",
+    "http://167.88.46.48:3100",
+  ]
+}
 
 export default nextConfig;
 
