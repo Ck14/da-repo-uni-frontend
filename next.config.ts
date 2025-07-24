ce2a3eba6b45b5bac0138822cedc3d29acc26733
Ckevyn Omar Ovalle Sequén <covalle@minfin.gob.gt>
2025-07-24 17:22:53 -0600

    (update) env develp

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Otras opciones de configuración...
+
+  /**
+   * Evita advertencias de CORS en entorno de desarrollo
+   * Agrega las IPs o dominios que usarás para acceder a tu app
+   */
+  allowedDevOrigins: [
+    "http://localhost:3000",
+    "http://167.88.46.48:3000"
+  ]
 };
 
 export default nextConfig;
